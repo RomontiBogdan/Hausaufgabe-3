49ca2d705ec26e5a793d8854055d270ba2b52110
RomontiBogdan <romontibogdan@gmail.com>
2021-11-04 11:43:09 +0200

    Entities added

---
+++ src/Entities/Course.java
@@ -0,0 +1,10 @@
+package Entities;
+
+import java.util.List;
+
+public class Course {
+    private String name;
+    private Person teacher;
+    private List<Student> studentsEnrolled;
+    private int credits;
+}
---
+++ src/Entities/Teacher.java
@@ -0,0 +1,19 @@
+package Entities;
+
+import java.util.List;
+
+public class Teacher extends Person {
+    private List<Course> courses;
+    public Teacher(String firstName, String lastName, List<Course> courses) {
+        super(firstName, lastName);
+        this.courses = courses;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+}
